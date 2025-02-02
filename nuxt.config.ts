60649d1227a07b513e5beb397a744cbdde94b6cc
Eduardo_AmengualGarcia <eduardo.amengualgarcia@dellteam.com>
2025-02-02 11:42:48 -0300

    feat: added satoshi font & started reset css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,5 +8,17 @@ export default defineNuxtConfig({
     extraPlugins: {
       scrollTrigger: true
     }
+  },
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://api.fontshare.com/v2/css?f[]=satoshi@400,401,500,501,700&display=swap"
+        }
+      ]
+    }
   }
 })
