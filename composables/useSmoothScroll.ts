8fae82977b70bdf7c0abe6c1afb31df8f2094863
Eduardo_AmengualGarcia <eduardo.amengualgarcia@dellteam.com>
2025-02-02 11:19:55 -0300

    feat: gsap setup and lenis with scrolltrigger setup

--- composables/useSmoothScroll.ts
+++ composables/useSmoothScroll.ts
@@ -1,11 +1,20 @@
 import Lenis from "lenis";
 
 export const useSmoothScroll = () => {
+    const { $gsap, $ScrollTrigger } = useNuxtApp();
+
     const lenis = new Lenis({
         autoRaf: true,
     });
 
     lenis.on('scroll', (e) => {
-        console.log(e);
+        // console.log(e);
+        $ScrollTrigger.update();
     });
+
+    $gsap.ticker.add((time : number) => {
+        lenis.raf(time * 1000)
+    })
+
+    $gsap.ticker.lagSmoothing(0);
 }
