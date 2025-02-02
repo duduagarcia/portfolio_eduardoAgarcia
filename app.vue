b42346b99a436f770634702c4bb938c6993c2d25
Eduardo_AmengualGarcia <eduardo.amengualgarcia@dellteam.com>
2025-02-02 13:33:15 -0300

    feat: console name & scale system

--- app.vue
+++ app.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 onMounted(() => {
   useSmoothScroll()
+  useConsoleName()
 })
 </script>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
         <h1>Index page</h1>
+        <h2>Index page</h2>
     </div>
 </template>
